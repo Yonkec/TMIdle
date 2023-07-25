275b3eebf922fa908317b82199a7e8ed3c531b87
Jason Wilson <jason@yonkec.com>
2023-07-24 21:11:02 -0400

    lodash and crude state machine skeleton

--- src/main.js
+++ src/main.js
@@ -11,7 +11,53 @@ import { buyMalker } from "./generators.js";
 //references necessary for function calls from modules
 window.openTab = openTab;
 
- 
+
+//state machine testing
+
+const changeStateButton = document.getElementById('changeState');
+
+const myStateMachine = new machina.Fsm({
+    initialState: 'paused',
+    states: {
+    paused: {
+        changeState: 'running',
+        _onEnter() {
+        console.log('State changed to paused');
+        }
+    },
+    running: {
+        changeState: 'paused',
+        _onEnter() {
+        console.log('State changed to running');
+        }
+    }
+    },
+    // Custom handler to update the button text based on the current state
+    buttonTextHandler(state) {
+        changeStateButton.textContent = state;
+    }
+});
+
+// Function to update the button text based on the current state
+function updateButtonText(state) {
+    const changeStateButton = document.getElementById('changeState');
+    changeStateButton.textContent = state;
+}
+
+// Add a click event listener to the button
+changeStateButton.addEventListener('click', () => {
+myStateMachine.handle('changeState');
+});
+
+// Call the updateButtonText function initially to set the initial button text
+updateButtonText(myStateMachine.state);
+
+// Set up the _onChange callback to update button text on state change
+myStateMachine.on('transition', function () {
+updateButtonText(myStateMachine.state);
+});
+
+
 //initialize game objects
 let player = new Player();
 let mob = new Enemy(50, player);
@@ -48,8 +94,3 @@ window.setInterval(function(){
 window.setInterval(function(){
 	mob.applyDMG(player.malkers); //need to transition to a state machine and create/update mob objects in the play state
 }, 1000);
-
-
-// window.setInterval(function(){
-//     saveGame(malk, malkers);
-// }, 10000);
